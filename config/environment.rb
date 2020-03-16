8e9a069bd5acaa1ff73dd7a0e60531881f650db2
Jen McQuade <jenmcquade@gmail.com>
2020-03-16 12:29:29 -0700

    Add environment settings for SendGrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address => 'smpt.sendgrid.net',
+  :port => '587',
+  :authentication => :plain,
+  :user_name => Rails.application.credentials.sendgrid[:username],
+  :password => Rails.application.credentials.sendgrid[:password],
+  :domain => 'heroku.com',
+  :enable_starttls_auto => true
+}
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.hosts << "ubuntu1910"
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -60,6 +62,9 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-    # Add Rack::LiveReload to the bottom of the middleware stack with the default options:
-    config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload
+  # Add Rack::LiveReload to the bottom of the middleware stack with the default options:
+  config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload
+
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'http://ubuntu1910:3000' }
 end
